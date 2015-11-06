69b1cef14bcafb701739b6a13960fa1d44275903
tetor <tetor.takashi.etou@gmail.com>
2015-11-07 01:04:12 +0900

    Cannot connect AMD and bower

--- src/math/ceil.js
+++ src/math/ceil.js
@@ -1,15 +1,17 @@
 ;(function() {
   'use strict';
 
+  // FIXME: NOT WORK
   var math = require('mout/math');
 
-  // TODO: make function returns function
-  // angular.filter('nmCeil', wrap(math.ceil, [val]));
+  // TODO: make a function that returns a function
+  //       angular.filter('nmCeil', wrap(math.ceil, [val]));
   function ceil(val) {
-    return function() {
+    return function(val) {
       return math.ceil(val);
     };
   }
+
 
   angular
     .module('ngMout')
